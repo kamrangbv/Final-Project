70a5c64881b54daa01ceabbb9253dec922b4018b
kamrangbv <kurbanov_03@mail.ru>
2023-05-28 15:53:02 +0400

    corrected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
-var burgerMenu = document.getElementById('burger-menu');
-var overlay = document.getElementById('menu');
-burgerMenu.addEventListener('click', function () {
-    this.classList.toggle("close");
-    overlay.classList.toggle("overlay");
+// var burgerMenu = document.getElementById('burger-menu');
+// var overlay = document.getElementById('menu');
+// burgerMenu.addEventListener('click', function () {
+//     this.classList.toggle("close");
+//     overlay.classList.toggle("overlay");
+// });
+
+var toggleButton = document.querySelector('.toggle-menu');
+var navBar = document.querySelector('.nav-bar');
+toggleButton.addEventListener('click', function () {
+    navBar.classList.toggle('toggle');
 });
